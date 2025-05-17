128ea635c4370a9935c9b9aee195ff54edca21b0
Giles Smart <3227792+gilessmart@users.noreply.github.com>
2025-05-17 17:50:20 +0100

    Add test script

---
+++ test.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# Tests the behavior of the git-url.sh script
+
+# Usage: ./test.sh
+
+test() {
+    description=$1
+    command=$2
+    expected=$3
+    
+    echo "Test:     $description"
+    echo "Expected: $expected"
+
+    result=$($command 2>&1)
+    if [ $? -ne 0 ]; then
+        echo "git-url.sh failed to execute command - $command - $result"
+        return 1
+    fi
+
+    echo "Actual:   $result"
+
+    if [ "$expected" = "$result" ]; then
+        echo "Result:   Pass"
+    else
+        echo "Result:   Fail"
+        return 1
+    fi
+}
+
+failed_tests=0
+
+commit_hash=$(git rev-parse --short HEAD)
+
+description="With no options"
+command="./git-url.sh README.md"
+expected="https://github.com/gilessmart/git-url-gen/blob/$commit_hash/README.md"
+test "$description" "$command" "$expected" || failed_tests=$(($failed_tests + 1))
+
+description="With line number option"
+command="./git-url.sh -l 42 README.md"
+expected="https://github.com/gilessmart/git-url-gen/blob/$commit_hash/README.md#L42"
+test "$description" "$command" "$expected" || failed_tests=$(($failed_tests + 1))
+
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+
+description="With branch option"
+command="./git-url.sh -b README.md"
+expected="https://github.com/gilessmart/git-url-gen/blob/$branch_name/README.md"
+test "$description" "$command" "$expected" || failed_tests=$(($failed_tests + 1))
+
+description="With branch and line number options"
+command="./git-url.sh -b -l 42 README.md"
+expected="https://github.com/gilessmart/git-url-gen/blob/$branch_name/README.md#L42"
+test "$description" "$command" "$expected" || failed_tests=$(($failed_tests + 1))
+
+if [ $failed_tests -eq 0 ]; then
+    echo "All tests passed"
+else
+    echo "$failed_tests test(s) failed"
+    exit 1
+fi
