e8f4df39f99bf3942259fb702229eb21ee4adcc2
Giles Smart <3227792+gilessmart@users.noreply.github.com>
2025-05-22 10:47:54 +0100

    Update tests to match the updated test files

--- test.sh
+++ test.sh
@@ -37,40 +37,35 @@ failed_tests=0
 commit_hash=$(git rev-parse --short HEAD)
 
 description="With no options"
-command="./git-url.sh README.md"
-expected="https://github.com/gilessmart/git-url-gen/blob/$commit_hash/README.md"
+command="./git-url.sh test-files/example.txt"
+expected="https://github.com/gilessmart/git-url-gen/blob/$commit_hash/test-files/example.txt"
 test "$description" "$command" "$expected" || failed_tests=$(($failed_tests + 1))
 
 description="With line number option"
-command="./git-url.sh -l 42 README.md"
-expected="https://github.com/gilessmart/git-url-gen/blob/$commit_hash/README.md#L42"
+command="./git-url.sh -l 5 test-files/example.txt"
+expected="https://github.com/gilessmart/git-url-gen/blob/$commit_hash/test-files/example.txt#L5"
 test "$description" "$command" "$expected" || failed_tests=$(($failed_tests + 1))
 
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 
 description="With branch option"
-command="./git-url.sh -b README.md"
-expected="https://github.com/gilessmart/git-url-gen/blob/$branch_name/README.md"
+command="./git-url.sh -b test-files/example.txt"
+expected="https://github.com/gilessmart/git-url-gen/blob/$branch_name/test-files/example.txt"
 test "$description" "$command" "$expected" || failed_tests=$(($failed_tests + 1))
 
 description="With branch and line number options"
-command="./git-url.sh -b -l 42 README.md"
-expected="https://github.com/gilessmart/git-url-gen/blob/$branch_name/README.md#L42"
+command="./git-url.sh -b -l 5 test-files/example.txt"
+expected="https://github.com/gilessmart/git-url-gen/blob/$branch_name/test-files/example.txt#L5"
 test "$description" "$command" "$expected" || failed_tests=$(($failed_tests + 1))
 
-description="With a file name with various special characters"
-command="./git-url.sh 'test-files/¬£%^&()_+-=[]{};:@#~<>,.?|'"
-expected="https://github.com/gilessmart/git-url-gen/blob/$commit_hash/test-files/%C2%AC%C2%A3%25%5E%26()_%2B-%3D%5B%5D%7B%7D%3B%3A%40%23~%3C%3E%2C.%3F%7C"
+description="With a file name with special characters"
+command="./git-url.sh 'test-files/¬\`!£$%^&()-_=+[]{};'\''@#~, .txt'"
+expected="https://github.com/gilessmart/git-url-gen/blob/$commit_hash/test-files/%C2%AC%60!%C2%A3%24%25%5E%26()-_%3D%2B%5B%5D%7B%7D%3B'%40%23~%2C%20.txt"
 test "$description" "$command" "$expected" || failed_tests=$(($failed_tests + 1))
 
-description="With a file name with spaces"
-command="./git-url.sh 'test-files/file with spaces'"
-expected="https://github.com/gilessmart/git-url-gen/blob/$commit_hash/test-files/file%20with%20spaces"
-test "$description" "$command" "$expected" || failed_tests=$(($failed_tests + 1))
-
-description="With a file name with extra-special characters"
-command="./git-url.sh 'test-files/\`!\"'\''\$*\'"
-expected="https://github.com/gilessmart/git-url-gen/blob/$commit_hash/test-files/%60!%22'%24*%5C"
+description="With a file name with 'extra-special' characters"
+command="./git-url.sh 'test-files/<>:\"\\|?*.txt'"
+expected="https://github.com/gilessmart/git-url-gen/blob/$commit_hash/test-files/%3C%3E%3A%22%5C%7C%3F*.txt"
 test "$description" "$command" "$expected" || failed_tests=$(($failed_tests + 1))
 
 if [ $failed_tests -eq 0 ]; then
