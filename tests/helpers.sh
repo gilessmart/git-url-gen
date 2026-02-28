960fe57ec57bccb7dd0c91666ce70e0755c1dde0
gilessmart <3227792+gilessmart@users.noreply.github.com>
2026-02-28 19:06:50 +0000

    Stop leaking globals in test helper functions

--- tests/helpers.sh
+++ tests/helpers.sh
@@ -3,10 +3,10 @@ export TEST_REPOS_DIR="$SCRIPT_DIR/test-repos"
 export GITURL_PATH="$(realpath $SCRIPT_DIR/../giturl.sh)"
 
 setup_test_repo() {
-    dir_path=$1
-    remote_url=$2
-    branch=$3
-    clear_repos=$4
+    local dir_path=$1
+    local remote_url=$2
+    local branch=$3
+    local clear_repos=$4
 
     if $clear_repos; then
         printf 'Removing test repo: %s...\n' "$dir_path"
@@ -21,13 +21,13 @@ setup_test_repo() {
 }
 
 test() {
-    description=$1
-    command=$2
-    expected=$3
+    local description=$1
+    local command=$2
+    local expected=$3
 
     printf 'Test:     %s\n' "$description"
 
-    result=$(eval $command 2>&1)
+    local result=$(eval $command 2>&1)
     if [[ $? -ne 0 ]]; then
         printf 'Failed to execute command\nCommand: %s\nResult: %s\n\n' "$command" "$result"
         return 1
