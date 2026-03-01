57b80367455b322238adcfbf4bb8cab845d78670
gilessmart <3227792+gilessmart@users.noreply.github.com>
2026-03-01 18:18:03 +0000

    Add test for https remote URLs

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -18,63 +18,70 @@ getopts ":c" opt || true
 clear_repos=false
 [[ $opt = c ]] && clear_repos=true
 
-setup_test_repo main-branch git@github.com:gilessmart/giturl.git main $clear_repos
-setup_test_repo slash-branch git@github.com:gilessmart/giturl.git 'test-branches/abc' $clear_repos
-setup_test_repo special-chars-branch git@github.com:gilessmart/giturl.git 'test-branches/_=+,.@¬£' $clear_repos
+setup_test_repo ssh-main git@github.com:gilessmart/giturl.git main $clear_repos
+setup_test_repo ssh-slash-branch git@github.com:gilessmart/giturl.git 'test-branches/abc' $clear_repos
+setup_test_repo ssh-special-chars-branch git@github.com:gilessmart/giturl.git 'test-branches/_=+,.@¬£' $clear_repos
+setup_test_repo https-main https://github.com/gilessmart/giturl.git main $clear_repos
 printf '\n'
 
 failed_tests=0
 
-main_branch_hash=$(cd "$TEST_REPOS_DIR/main-branch" && git rev-parse --short HEAD)
+ssh_main_hash=$(cd "$TEST_REPOS_DIR/ssh-main" && git rev-parse --short HEAD)
+https_main_hash=$(cd "$TEST_REPOS_DIR/https-main" && git rev-parse --short HEAD)
 
 description="With root level folder path"
-command="'$GITURL_PATH' '$TEST_REPOS_DIR/main-branch'"
-expected="https://github.com/gilessmart/giturl/blob/$main_branch_hash"
+command="'$GITURL_PATH' '$TEST_REPOS_DIR/ssh-main'"
+expected="https://github.com/gilessmart/giturl/blob/$ssh_main_hash"
 test "$description" "$command" "$expected" || ((++failed_tests))
 
 description="With root level file path"
-command="'$GITURL_PATH' '$TEST_REPOS_DIR/main-branch/README.md'"
-expected="https://github.com/gilessmart/giturl/blob/$main_branch_hash/README.md"
+command="'$GITURL_PATH' '$TEST_REPOS_DIR/ssh-main/README.md'"
+expected="https://github.com/gilessmart/giturl/blob/$ssh_main_hash/README.md"
 test "$description" "$command" "$expected" || ((++failed_tests))
 
 description="With nested folder path"
-command="'$GITURL_PATH' '$TEST_REPOS_DIR/main-branch/tests/test-files'"
-expected="https://github.com/gilessmart/giturl/blob/$main_branch_hash/tests/test-files"
+command="'$GITURL_PATH' '$TEST_REPOS_DIR/ssh-main/tests/test-files'"
+expected="https://github.com/gilessmart/giturl/blob/$ssh_main_hash/tests/test-files"
 test "$description" "$command" "$expected" || ((++failed_tests))
 
 description="With nested file path"
-command="'$GITURL_PATH' '$TEST_REPOS_DIR/main-branch/tests/test-files/example.txt'"
-expected="https://github.com/gilessmart/giturl/blob/$main_branch_hash/tests/test-files/example.txt"
+command="'$GITURL_PATH' '$TEST_REPOS_DIR/ssh-main/tests/test-files/example.txt'"
+expected="https://github.com/gilessmart/giturl/blob/$ssh_main_hash/tests/test-files/example.txt"
 test "$description" "$command" "$expected" || ((++failed_tests))
 
 description="With line number option"
-command="'$GITURL_PATH' -l 5 '$TEST_REPOS_DIR/main-branch/tests/test-files/example.txt'"
-expected="https://github.com/gilessmart/giturl/blob/$main_branch_hash/tests/test-files/example.txt#L5"
+command="'$GITURL_PATH' -l 5 '$TEST_REPOS_DIR/ssh-main/tests/test-files/example.txt'"
+expected="https://github.com/gilessmart/giturl/blob/$ssh_main_hash/tests/test-files/example.txt#L5"
 test "$description" "$command" "$expected" || ((++failed_tests))
 
 description="With branch option"
-command="'$GITURL_PATH' -b '$TEST_REPOS_DIR/main-branch/tests/test-files/example.txt'"
+command="'$GITURL_PATH' -b '$TEST_REPOS_DIR/ssh-main/tests/test-files/example.txt'"
 expected="https://github.com/gilessmart/giturl/blob/main/tests/test-files/example.txt"
 test "$description" "$command" "$expected" || ((++failed_tests))
 
 description="With branch and line number options"
-command="'$GITURL_PATH' -b -l 5 '$TEST_REPOS_DIR/main-branch/tests/test-files/example.txt'"
+command="'$GITURL_PATH' -b -l 5 '$TEST_REPOS_DIR/ssh-main/tests/test-files/example.txt'"
 expected="https://github.com/gilessmart/giturl/blob/main/tests/test-files/example.txt#L5"
 test "$description" "$command" "$expected" || ((++failed_tests))
 
 description="With a file name with special characters"
-command="'$GITURL_PATH' '$TEST_REPOS_DIR/main-branch/tests/test-files/example-_=+,.@¬£.txt'"
-expected="https://github.com/gilessmart/giturl/blob/$main_branch_hash/tests/test-files/example-_%3D%2B%2C.%40%C2%AC%C2%A3.txt"
+command="'$GITURL_PATH' '$TEST_REPOS_DIR/ssh-main/tests/test-files/example-_=+,.@¬£.txt'"
+expected="https://github.com/gilessmart/giturl/blob/$ssh_main_hash/tests/test-files/example-_%3D%2B%2C.%40%C2%AC%C2%A3.txt"
 test "$description" "$command" "$expected" || ((++failed_tests))
 
 description="With branch with a slash in its name"
-command="'$GITURL_PATH' -b '$TEST_REPOS_DIR/slash-branch/tests/test-files/example.txt'"
+command="'$GITURL_PATH' -b '$TEST_REPOS_DIR/ssh-slash-branch/tests/test-files/example.txt'"
 expected="https://github.com/gilessmart/giturl/blob/test-branches/abc/tests/test-files/example.txt"
 test "$description" "$command" "$expected" || ((++failed_tests))
 
 description="With branch with special characters in its name"
-command="'$GITURL_PATH' -b '$TEST_REPOS_DIR/special-chars-branch/tests/test-files/example.txt'"
+command="'$GITURL_PATH' -b '$TEST_REPOS_DIR/ssh-special-chars-branch/tests/test-files/example.txt'"
 expected="https://github.com/gilessmart/giturl/blob/test-branches/_%3D%2B%2C.%40%C2%AC%C2%A3/tests/test-files/example.txt"
+test "$description" "$command" "$expected" || ((++failed_tests))
+
+description="With https remote URL"
+command="'$GITURL_PATH' '$TEST_REPOS_DIR/https-main/tests/test-files/example.txt'"
+expected="https://github.com/gilessmart/giturl/blob/$https_main_hash/tests/test-files/example.txt"
 test "$description" "$command" "$expected" || ((++failed_tests))
 
 # check the results return 200 with:
